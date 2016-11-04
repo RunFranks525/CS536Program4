dc9d8265de1ae9b49d39f97f6b359aab08dc8d11
lewiskunik <lkunik@wisc.edu>
2016-11-04 17:43:45 -0500

    modified SemSym.java

--- SemSym.java
+++ SemSym.java
@@ -13,3 +13,50 @@ public class SemSym {
         return type;
     }
 }
+
+public class StructSym extends SemSym{
+
+	private HashMap<String, SemSym> fields;
+	
+	public StructSym(String type) {
+		this.type = type;
+		fields = new HashMap<String, SemSym>();
+	}
+
+	public void addField(String name, SemSym symbol){
+		fields.put(name, symbol);
+	}
+	
+	public Boolean contains(String name){
+		if(fields.containsKey(name))
+			return true;
+		else
+			return false;
+	}
+}
+
+
+public class FuncSym extends SemSym{
+
+	private ArrayList<SemSym> args;
+
+	public FuncSym(String type){
+		this.type = type;
+		args = new List<String, SemSym>();
+	}
+	
+	public void addArg(SemSym symbol){
+		args.add(symbol);
+	}
+
+	public void numArgs(){
+		return args.size();
+	}
+
+	public ArrayList<SemSym> getArgs(){
+		return args;
+	} 
+	
+
+
+}
