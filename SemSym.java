9bc9e6c29a9c97e5a0a57dd51fe785881ec392cf
Scott Franklin <stfranklin@wisc.edu>
2016-11-04 18:32:27 -0500

    Fixing some schtuff in SemSym

--- SemSym.java
+++ SemSym.java
@@ -1,62 +1,73 @@
 public class SemSym {
-    private String type;
-    
+    protected String type;
+
     public SemSym(String type) {
         this.type = type;
     }
-    
+
     public String getType() {
         return type;
     }
-    
+
     public String toString() {
         return type;
     }
 }
 
+<<<<<<< HEAD
 public class StructSym extends SemSym{
 	@Override
+=======
+class StructSym extends SemSym{
+
+>>>>>>> Fixing some schtuff in SemSym
 	private HashMap<String, SemSym> fields;
-	
+
 	public StructSym(String type) {
-		this.type = type;
+		super(type);
 		fields = new HashMap<String, SemSym>();
 	}
 
 	public void addField(String name, SemSym symbol){
 		fields.put(name, symbol);
 	}
-	
+
 	public Boolean contains(String name){
-		if(fields.containsKey(name))
-			return true;
-		else
-			return false;
+		if(fields.containsKey(name)) {
+      return true;
+    }
+		else {
+      return false;
+    }
 	}
 }
 
 
-public class FuncSym extends SemSym{
+class FuncSym extends SemSym{
 
 	private ArrayList<SemSym> args;
 
-	public FuncSym(String type){
-		this.type = type;
+	public FuncSym(String type) {
+    super(type);
 		args = new List<String, SemSym>();
 	}
-	
-	public void addArg(SemSym symbol){
+
+	public void addArg(SemSym symbol) {
 		args.add(symbol);
 	}
 
+<<<<<<< HEAD
 	public int numArgs(){
+=======
+	public void numArgs() {
+>>>>>>> Fixing some schtuff in SemSym
 		return args.size();
 	}
 
 	public ArrayList<SemSym> getArgs(){
 		return args;
-	} 
-	
+	}
+
 
 
 }
